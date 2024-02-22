0d6544a3835280327fae45bd0f99c32d986b80e7
Romte1 <ivan_ni224@hotmail.com>
2024-02-22 00:52:37 -0300

    Done some early console testings, nothing to show yet

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+** The Gameboard represents the state of the board
+*/
+
+function Gameboard() {
+    const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const getBoard = () => board;
+    return getBoard();
+}
+const board = Gameboard();
+
+
+function TicTacToe() {
+    const board = Gameboard();
+
+}
